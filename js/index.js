d4d91b1e57c7a5495db8921e455086cf462ba4ec
rileymeye1 <rileymeye1@gmail.com>
2023-07-16 19:05:11 +0300

    Create a way to keep score and track the round
    I created a variable for the playerScore, computerScore and gameRound.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+//create a variable playerSelection to store the Users choice
+const playerSelection = prompt('Please choose Rock, Paper or Scissors?')
+console.log("This is the Player's selection: " + playerSelection);
+
 //create a function called getComputerChoice that randomly returns 'Rock', 'Paper', or 'Scissors'
 function getComputerChoice() {
-    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
+    let choices = ['Rock', 'Paper', 'Scissors'];
     let selection = choices[Math.floor(Math.random()*choices.length)];
     return selection;
 }
@@ -9,9 +13,17 @@ function getComputerChoice() {
 const computerSelection = getComputerChoice();
 console.log("This is Computer's selection: " + computerSelection);
 
-//create a variable playerSelection to store the Users choice
-const playerSelection = 'Rock';
-console.log("This is the Player's selection: " + playerSelection);
+//create a variable to store the player's score
+let playerScore = 0;
+console.log(playerScore);
+
+//create a variable to store the computer's score
+let computerScore = 0;
+console.log(computerScore);
+
+//create a variable to store the Round you are currently on.
+let gameRound = 0;
+console.log(gameRound);
 
 //create a function that plays a single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
@@ -22,25 +34,24 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (player == 'ROCK') {
         if (computer == 'SCISSORS') {
-            return "You Won! ROCK beats SCISSORS.";
+            return "You Won! Rock beats Scissors.";
         }else{
-            return "You Lose! PAPER beats ROCK.";
+            return "You Lose! Paper beats Rock.";
         }
     }
     else if (player == 'PAPER') {
         if (computer == 'ROCK') {
-            return "You Won! PAPER beats ROCK.";
+            return "You Won! Paper beats Rock.";
         }else{
-            return "You Lose! SCISSORS beats PAPER.";
+            return "You Lose! Scissors beats Paper.";
         }
     }
     else if (player == 'SCISSORS') {
         if (computer == 'PAPER') {
-            return "You Won! SCISSORS beats PAPER.";
+            return "You Won! Scissors beats Paper.";
         }else{
-            return "You Lose! ROCK beats SCISSORS.";
+            return "You Lose! Rock beats Scissors.";
         }
     }
 }
-
 console.log(playRound(playerSelection, computerSelection));
