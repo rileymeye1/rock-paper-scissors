15cddc2d68afb5b3ea1215a9dfb97916cd807343
rileymeye1 <rileymeye1@gmail.com>
2023-07-16 20:42:55 +0300

    Add comments to all the code
    I went back and added comments to each variable, function, and loop to
    remind myself what each section does for future use and changes.

--- js/index.js
+++ js/index.js
@@ -1,35 +1,47 @@
+//creates the playerScore variable to store the player's score.
 let playerScore = 0;
 console.log("Player's current score is: " + playerScore);
 
+//creates the computerScore variable to store the computer's score.
 let computerScore = 0;
 console.log("Computer's current score is: " + computerScore);
 
+//creates the gameRound variable to store the number of rounds played.
 let gameRound = 0;
 console.log("You have played " + gameRound + " rounds.");
 
+//used a while loop to run the game function 5 times.
 while (gameRound < 5) {
     game();
 }
 
+//game function stores all the game code.
 function game() {
 
+//creates a playerSelection variable to store the player's choice for the round.
 const playerSelection = getPlayerChoice();
 
+//a function that prompts the player to make a choice.
 function getPlayerChoice() {
     playerTurn = prompt('Please choose Rock, Paper or Scissors?');
     return playerTurn;
 }
 
+//creates a computerSelection variable to store the computer's choice for the round.
 const computerSelection = getComputerChoice();
 
+//a function that randomly chooses from a set list to make the computer's choice.
 function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors'];
     let computerTurn = choices[Math.floor(Math.random()*choices.length)];
     return computerTurn;
 }
 
+//triggers the playRound function.
 playRound(playerSelection, computerSelection);
 
+/*a function that converts the playerSelection and computerSelection to all UPPERCASE letters.
+It then compares the player and computer to determine a winner and increases the winner's score after each round.*/
 function playRound(playerSelection, computerSelection) {
     player = playerSelection.toUpperCase();
     computer = computerSelection.toUpperCase();
@@ -80,6 +92,7 @@ console.log("You have now played " + gameRound + " rounds.");
 
 }
 
+//a function to determine the winner of the game after 5 rounds.
 function determineWinner(playerScore, computerScore) {
     if (playerScore === computerScore) {
         return "It's a TIE! GAME OVER"
