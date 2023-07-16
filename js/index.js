7259f1bfc1126780edfd5c9d6c7101695205daa5
rileymeye1 <rileymeye1@gmail.com>
2023-07-16 18:46:08 +0300

    Create a function that plays a single round
    I created a function that plays a single round of Rock, Paper, Scissors
    by converting the playerSelection and computerSelection variables
    toUpperCase. It then compares the player and computer to see if they
    match and end in a tie. If it is not a tie I then use if and else if
    statements to compare the player and computer and determine a winner!

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //create a function called getComputerChoice that randomly returns 'Rock', 'Paper', or 'Scissors'
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors'];
+    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
     let selection = choices[Math.floor(Math.random()*choices.length)];
     return selection;
 }
@@ -10,13 +10,37 @@ const computerSelection = getComputerChoice();
 console.log("This is Computer's selection: " + computerSelection);
 
 //create a variable playerSelection to store the Users choice
-// let playerSelection = null;
+const playerSelection = 'Rock';
+console.log("This is the Player's selection: " + playerSelection);
 
-//create a variable computerSelection to store the Computers choice
+//create a function that plays a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    player = playerSelection.toUpperCase();
+    computer = computerSelection.toUpperCase();
+    if (player === computer) {
+        return "It's a tie!";
+    }
+    else if (player == 'ROCK') {
+        if (computer == 'SCISSORS') {
+            return "You Won! ROCK beats SCISSORS.";
+        }else{
+            return "You Lose! PAPER beats ROCK.";
+        }
+    }
+    else if (player == 'PAPER') {
+        if (computer == 'ROCK') {
+            return "You Won! PAPER beats ROCK.";
+        }else{
+            return "You Lose! SCISSORS beats PAPER.";
+        }
+    }
+    else if (player == 'SCISSORS') {
+        if (computer == 'PAPER') {
+            return "You Won! SCISSORS beats PAPER.";
+        }else{
+            return "You Lose! ROCK beats SCISSORS.";
+        }
+    }
+}
 
-
-//create a function called getPlayerChoice where the User can choose 'Rock', 'Paper', or 'Scissors'
-
-//create a function that compares getPlayerChoice with getComputerChoice to determine a winner and returns a string; ensure the playerSelection parameter is case-insensitive
-//ensure you return the results of the function call
-
+console.log(playRound(playerSelection, computerSelection));
